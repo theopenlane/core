120847a38f705111a36ad1dc4d34b7340c1c1825
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-09-23 07:50:31 -0600

    Remove mod=mod from go run commands in generate (#80)
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- generate.go
+++ generate.go
@@ -1,13 +1,13 @@
 package main
 
 //go:generate echo "------> Generating code - running entc.go... <------"
-//go:generate go run -mod=mod ./internal/ent/entc.go
+//go:generate go run ./internal/ent/entc.go
 //go:generate echo "------> Generating code - running gqlgen... <------"
 //go:generate go run ./internal/graphapi/generate/generate.go
 //go:generate echo "------> Generating code - running gen_schema.go... <------"
-//go:generate go run -mod=mod ./gen_schema.go
+//go:generate go run ./gen_schema.go
 //go:generate echo "------> Generating code - running gqlgenc... <------"
-//go:generate go run -mod=mod github.com/Yamashou/gqlgenc generate --configdir schema
+//go:generate go run github.com/Yamashou/gqlgenc generate --configdir schema
 //go:generate echo "------> Tidying up... <------"
 //go:generate go mod tidy
 //go:generate echo "------> Code generation process completed! <------"
