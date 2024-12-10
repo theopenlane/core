79ddc29765a4029f58c39e459be364977c039920
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-12-10 16:55:28 -0700

    cleanup generate
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- generate_gql.go
+++ generate_gql.go
@@ -1,12 +1,12 @@
 package main
 
 //go:generate echo "------> Generating code - running gqlgen... <------"
-//go:generate_input  ./internal/graphapi/generate/generate.go internal/graphapi/*.go
+//go:generate_input  internal/graphapi/generate/generate.go internal/graphapi/*.go
 //go:generate_output internal/graphapi/*.resolvers.go internal/graphapi/gen_models.go internal/graphapi/gen_server.go pkg/openlaneclient/models.go
 //go:generate go run ./internal/graphapi/generate/generate.go
 
 //go:generate echo "------> Generating code - running gen_schema.go... <------"
-//go:generate_input ./gen_schema.go internal/graphapi/*.go
+//go:generate_input gen_schema.go internal/graphapi/*.go
 //go:generate_output schema.graphql
 //go:generate go run ./gen_schema.go
 
