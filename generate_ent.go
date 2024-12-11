ddb6b4732ec0439e986e5c8d445d1b30bf449e02
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-12-10 21:58:53 -0700

    fixup the generate
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- generate_ent.go
+++ generate_ent.go
@@ -1,10 +1,5 @@
 package main
 
-//go:generate echo "------> Generating code - running entc.go... <------"
-//go:generate_input internal/ent/entc.go internal/ent/schema/*.go internal/ent/mixin/*.go internal/ent/templates/*.tmpl
-//go:generate_output internal/ent/generated
+//go:generate_input internal/ent/entc.go internal/ent/schema/* internal/ent/mixin/* internal/ent/templates/*
+//go:generate_output internal/ent/generated/*
 //go:generate go run ./internal/ent/entc.go
-
-//go:generate echo "------> Tidying up... <------"
-//go:generate go mod tidy
-//go:generate echo "------> Code generation process completed! <------"
