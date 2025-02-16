e2a9acc86a9ba6eff3736cdd6479d7456f8b9aa7
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-02-15 18:35:11 -0700

    feat: group visibility, managed group tags (#456)
    * feat: group visibility, managed group tags
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    * fix if there is no group associated with a group setting
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    * cleanup
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- cmd/cli/cmd/groupsetting/root.go
+++ cmd/cli/cmd/groupsetting/root.go
@@ -76,7 +76,16 @@ func jsonOutput(out any) error {
 func tableOutput(out []openlaneclient.GroupSetting) {
 	writer := tables.NewTableWriter(command.OutOrStdout(), "ID", "GroupName", "Visibility", "SyncToGithub", "SyncToSlack")
 	for _, i := range out {
-		writer.AddRow(i.ID, i.Group.Name, i.Visibility, *i.SyncToGithub, *i.SyncToSlack)
+		groupName := ""
+		if i.Group != nil {
+			groupName = i.Group.Name
+		}
+
+		writer.AddRow(i.ID,
+			groupName,
+			i.Visibility,
+			*i.SyncToGithub,
+			*i.SyncToSlack)
 	}
 
 	writer.Render()
