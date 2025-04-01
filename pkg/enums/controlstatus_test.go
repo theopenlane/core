f39149e78ac300409f011edcfd241e692a1eb14e
Lanre Adelowo <yo@lanre.wtf>
2025-04-01 21:08:26 +0100

    add enums tests

---
+++ pkg/enums/controlstatus_test.go
@@ -0,0 +1,49 @@
+package enums_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/theopenlane/core/pkg/enums"
+)
+
+func TestControlStatus(t *testing.T) {
+	testCases := []struct {
+		name     string
+		role     string
+		expected enums.ControlStatus
+	}{
+		{
+			name:     "preparing",
+			role:     "preparing",
+			expected: enums.ControlStatusPreparing,
+		},
+		{
+			name:     "needs approval",
+			role:     "needs approval",
+			expected: enums.ControlStatusNeedsApproval,
+		},
+		{
+			name:     "request changes",
+			role:     "changes requested",
+			expected: enums.ControlStatusChangesRequested,
+		},
+		{
+			name:     "approved",
+			role:     "approved",
+			expected: enums.ControlStatusApproved,
+		},
+		{
+			name:     "archived",
+			role:     "archived",
+			expected: enums.ControlStatusArchived,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res := enums.ToControlStatus(tc.role)
+			assert.Equal(t, tc.expected, *res)
+		})
+	}
+}
