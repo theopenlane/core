37cbe4603d04de4858b205e8f48fdcac38c6ec67
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-05-03 22:04:44 -0600

    testing
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- pkg/testutils/auth.go
+++ pkg/testutils/auth.go
@@ -3,12 +3,8 @@ package testutils
 import (
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
-	"fmt"
 	"io"
 	"log"
-	"os"
 	"time"
 
 	"github.com/alicebob/miniredis/v2"
@@ -16,7 +12,6 @@ import (
 
 	"github.com/theopenlane/iam/sessions"
 	"github.com/theopenlane/iam/tokens"
-	"github.com/theopenlane/utils/ulids"
 )
 
 // NewRedisClient creates a new redis client for testing using miniredis
@@ -65,60 +60,12 @@ func CreateTokenManager(refreshOverlap time.Duration) (*tokens.TokenManager, err
 		AccessDuration:  1 * time.Hour, //nolint:mnd
 		RefreshDuration: 2 * time.Hour, //nolint:mnd
 		RefreshOverlap:  refreshOverlap,
-		JWKSEndpoint:    "http://localhost:17608/.well-known/jwks.json",
 	}
 
-	// generate keys if needed
-	conf, err := generateKeys(conf)
+	key, err := rsa.GenerateKey(rand.Reader, 2048) //nolint:mnd
 	if err != nil {
-		log.Fatalf("Error generating keys: %v", err)
+		return nil, err
 	}
 
-	return tokens.New(conf)
-}
-
-func generateKeys(conf tokens.Config) (tokens.Config, error) {
-	privFileName := "../../../private_key.pem"
-
-	// generate a new private key if one doesn't exist
-	if _, err := os.Stat(privFileName); err != nil {
-		// Generate a new RSA private key with 2048 bits
-		privateKey, err := rsa.GenerateKey(rand.Reader, 2048) //nolint:mnd
-		if err != nil {
-			log.Fatalf("Error generating RSA private key")
-		}
-
-		// Encode the private key to the PEM format
-		privateKeyPEM := &pem.Block{
-			Type:  "RSA PRIVATE KEY",
-			Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
-		}
-
-		privateKeyFile, err := os.Create(privFileName)
-		if err != nil {
-			log.Fatalf("Error creating private key file")
-		}
-
-		if err := pem.Encode(privateKeyFile, privateKeyPEM); err != nil {
-			log.Fatalf("unable to encode pem on startup")
-		}
-
-		privateKeyFile.Close()
-	}
-
-	keys := map[string]string{}
-
-	// check if kid was passed in
-	kidPriv := conf.KID
-
-	// if we didn't get a kid in the settings, assign one
-	if kidPriv == "" {
-		kidPriv = ulids.New().String()
-	}
-
-	keys[kidPriv] = fmt.Sprintf("%v", privFileName)
-
-	conf.Keys = keys
-
-	return conf, nil
+	return tokens.NewWithKey(key, conf)
 }
