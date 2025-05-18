0865b3c0ff065ecb9a288bcb0a5ff72c411313d2
Lanre Adelowo <yo@lanre.wtf>
2025-05-18 20:04:29 +0100

    Add comments

--- internal/ent/privacy/token/job_runner.go
+++ internal/ent/privacy/token/job_runner.go
@@ -6,31 +6,37 @@ import (
 	"github.com/theopenlane/utils/contextx"
 )
 
+// JobRunnerRegistrationToken implements the PrivacyToken interface
 type JobRunnerRegistrationToken struct {
 	PrivacyToken
 	token string
 }
 
+// NewJobRunnerRegistrationToken creates a new PrivacyToken of type JobRunnerRegistrationToken
 func NewJobRunnerRegistrationToken(token string) JobRunnerRegistrationToken {
 	return JobRunnerRegistrationToken{
 		token: token,
 	}
 }
 
+// GetToken from the registration token
 func (token *JobRunnerRegistrationToken) GetToken() string {
 	return token.token
 }
 
+// SetToken sets the token
 func (token *JobRunnerRegistrationToken) SetToken(s string) {
 	token.token = s
 }
 
+// NewContextWithJobRunnerRegistrationToken  returns a new context with the job runner registration token
 func NewContextWithJobRunnerRegistrationToken(parent context.Context, token string) context.Context {
 	return contextx.With(parent, &JobRunnerRegistrationToken{
 		token: token,
 	})
 }
 
+// JobRunnerRegistrationTokenFromContext returns the registration token if available from the context
 func JobRunnerRegistrationTokenFromContext(ctx context.Context) *JobRunnerRegistrationToken {
 	token, ok := contextx.From[*JobRunnerRegistrationToken](ctx)
 	if !ok {
