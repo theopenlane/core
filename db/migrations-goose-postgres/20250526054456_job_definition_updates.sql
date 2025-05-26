fbb2226cb0eb03575159d1099e31fb4d783098fa
Lanre Adelowo <yo@lanre.wtf>
2025-05-26 13:12:25 +0100

    task db:resethash

--- db/migrations-goose-postgres/20250526054456_job_definition_updates.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
--- modify "control_scheduled_jobs" table
-ALTER TABLE "control_scheduled_jobs" DROP CONSTRAINT "control_scheduled_jobs_job_runners_job_runner", ALTER COLUMN "job_runner_id" DROP NOT NULL, ADD CONSTRAINT "control_scheduled_jobs_job_runners_job_runner" FOREIGN KEY ("job_runner_id") REFERENCES "job_runners" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
-
--- +goose Down
--- reverse: modify "control_scheduled_jobs" table
-ALTER TABLE "control_scheduled_jobs" DROP CONSTRAINT "control_scheduled_jobs_job_runners_job_runner", ALTER COLUMN "job_runner_id" SET NOT NULL, ADD CONSTRAINT "control_scheduled_jobs_job_runners_job_runner" FOREIGN KEY ("job_runner_id") REFERENCES "job_runners" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
--- db/migrations/20250526054453_job_definition_updates.sql
+++
@@ -1,2 +0,0 @@
--- Modify "control_scheduled_jobs" table
-ALTER TABLE "control_scheduled_jobs" DROP CONSTRAINT "control_scheduled_jobs_job_runners_job_runner", ALTER COLUMN "job_runner_id" DROP NOT NULL, ADD CONSTRAINT "control_scheduled_jobs_job_runners_job_runner" FOREIGN KEY ("job_runner_id") REFERENCES "job_runners" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
