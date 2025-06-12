bbd50a4f079b9c2399473231657903bdd1b95cd9
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2025-06-12 15:16:06 -0500

    add gcs support

--- pkg/objects/mocks/mocks.go
+++ pkg/objects/mocks/mocks.go
@@ -449,3 +449,60 @@ func (_c *MockStorage_Upload_Call) RunAndReturn(run func(context1 context.Contex
 	_c.Call.Return(run)
 	return _c
 }
+
+// Delete provides a mock function for the type MockStorage
+func (_mock *MockStorage) Delete(context1 context.Context, s string) error {
+	ret := _mock.Called(context1, s)
+
+	if len(ret) == 0 {
+		panic("no return value specified for Delete")
+	}
+
+	var r0 error
+	if returnFunc, ok := ret.Get(0).(func(context.Context, string) error); ok {
+		r0 = returnFunc(context1, s)
+	} else {
+		r0 = ret.Error(0)
+	}
+	return r0
+}
+
+// MockStorage_Delete_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'Delete'
+type MockStorage_Delete_Call struct {
+	*mock.Call
+}
+
+// Delete is a helper method to define mock.On call
+//   - context1 context.Context
+//   - s string
+func (_e *MockStorage_Expecter) Delete(context1 interface{}, s interface{}) *MockStorage_Delete_Call {
+	return &MockStorage_Delete_Call{Call: _e.mock.On("Delete", context1, s)}
+}
+
+func (_c *MockStorage_Delete_Call) Run(run func(context1 context.Context, s string)) *MockStorage_Delete_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		var arg1 string
+		if args[1] != nil {
+			arg1 = args[1].(string)
+		}
+		run(
+			arg0,
+			arg1,
+		)
+	})
+	return _c
+}
+
+func (_c *MockStorage_Delete_Call) Return(err error) *MockStorage_Delete_Call {
+	_c.Call.Return(err)
+	return _c
+}
+
+func (_c *MockStorage_Delete_Call) RunAndReturn(run func(context1 context.Context, s string) error) *MockStorage_Delete_Call {
+	_c.Call.Return(run)
+	return _c
+}
