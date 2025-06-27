3ee4b983f716d369c42b8dcb8931a467f6b7b86a
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2025-06-27 10:10:36 -0500

    linter

--- internal/httpserve/handlers/webhook.go
+++ internal/httpserve/handlers/webhook.go
@@ -18,7 +18,6 @@ import (
 	"github.com/theopenlane/iam/fgax"
 	"github.com/theopenlane/utils/contextx"
 
-	"github.com/theopenlane/core/internal/ent/generated"
 	ent "github.com/theopenlane/core/internal/ent/generated"
 	"github.com/theopenlane/core/internal/ent/generated/apitoken"
 	"github.com/theopenlane/core/internal/ent/generated/organization"
@@ -499,7 +498,7 @@ func (h *Handler) ensureFeatureTuples(ctx context.Context, orgID string, feats [
 	for _, f := range feats {
 		tuples = append(tuples, fgax.GetTupleKey(fgax.TupleRequest{
 			SubjectID:   orgID,
-			SubjectType: generated.TypeOrganization,
+			SubjectType: ent.TypeOrganization,
 			ObjectID:    f,
 			ObjectType:  "feature",
 			Relation:    "enabled",
