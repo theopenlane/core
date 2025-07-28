092743f1a056246e668c55347040c1cced1306b9
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-07-28 13:36:10 -0600

    fix: ensure org name uniqueness in tests
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- internal/graphapi/models_test.go
+++ internal/graphapi/models_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/theopenlane/core/pkg/enums"
 	"github.com/theopenlane/core/pkg/models"
 	"github.com/theopenlane/iam/auth"
+	"github.com/theopenlane/utils/ulids"
 )
 
 type OrganizationBuilder struct {
@@ -343,7 +344,7 @@ func randomName(t *testing.T) string {
 			b.WriteRune(r)
 		}
 	}
-	return b.String() + "_" + gofakeit.UUID()
+	return b.String() + "_" + ulids.New().String()
 }
 
 // DeleteClient is an interface for deleting entities
