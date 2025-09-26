cd5be84b31a32963f4b9657c6acadbc44a0896e4
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-09-26 11:21:50 -0600

    remove unused code
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- pkg/entitlements/portalsession.go
+++ pkg/entitlements/portalsession.go
@@ -7,28 +7,6 @@ import (
 	"github.com/stripe/stripe-go/v82"
 )
 
-// CreateBillingPortalUpdateSession generates an update session in stripe's billing portal which displays the customers current subscription tier and allows them to upgrade or downgrade
-func (sc *StripeClient) CreateBillingPortalUpdateSession(ctx context.Context, subsID, custID string) (*BillingPortalSession, error) {
-	params := &stripe.BillingPortalSessionCreateParams{
-		Customer:  &custID,
-		ReturnURL: &sc.Config.StripeBillingPortalSuccessURL,
-		FlowData: &stripe.BillingPortalSessionCreateFlowDataParams{
-			Type: stripe.String("subscription_update"),
-			SubscriptionUpdate: &stripe.BillingPortalSessionCreateFlowDataSubscriptionUpdateParams{
-				Subscription: &subsID,
-			},
-		},
-	}
-
-	billingPortalSession, err := sc.Client.V1BillingPortalSessions.Create(ctx, params)
-	if err != nil {
-		return nil, err
-	}
-
-	return &BillingPortalSession{
-		ManageSubscription: billingPortalSession.URL,
-	}, nil
-}
 
 // CreateBillingPortalPaymentMethods generates a session in stripe's billing portal which allows the customer to add / update payment methods
 func (sc *StripeClient) CreateBillingPortalPaymentMethods(ctx context.Context, custID string) (*BillingPortalSession, error) {
@@ -55,62 +33,4 @@ func (sc *StripeClient) CreateBillingPortalPaymentMethods(ctx context.Context, c
 	return &BillingPortalSession{
 		PaymentMethods: billingPortalSession.URL,
 	}, nil
-}
-
-// CancellationBillingPortalSession generates a session in stripe's billing portal which allows the customer to cancel their subscription
-// note: this does not work with subscription schedules
-func (sc *StripeClient) CancellationBillingPortalSession(ctx context.Context, subsID, custID string) (*BillingPortalSession, error) {
-	returnURL := fmt.Sprintf("%s?%s", sc.Config.StripeCancellationReturnURL, "cancelled=true")
-	params := &stripe.BillingPortalSessionCreateParams{
-		Customer:  &custID,
-		ReturnURL: &sc.Config.StripeBillingPortalSuccessURL, // this is the "return back to website" URL, not a cancellation / update specific one
-		FlowData: &stripe.BillingPortalSessionCreateFlowDataParams{
-			Type: stripe.String("subscription_cancel"),
-			SubscriptionCancel: &stripe.BillingPortalSessionCreateFlowDataSubscriptionCancelParams{
-				Subscription: &subsID,
-			},
-			AfterCompletion: &stripe.BillingPortalSessionCreateFlowDataAfterCompletionParams{
-				Type: stripe.String("redirect"),
-				Redirect: &stripe.BillingPortalSessionCreateFlowDataAfterCompletionRedirectParams{
-					ReturnURL: &returnURL,
-				},
-			},
-		},
-	}
-
-	billingPortalSession, err := sc.Client.V1BillingPortalSessions.Create(ctx, params)
-	if err != nil {
-		return nil, err
-	}
-
-	return &BillingPortalSession{
-		Cancellation: billingPortalSession.URL,
-	}, nil
-}
-
-// CreateBillingPortalAddModuleSession generates a billing portal session that allows
-// a customer to add a module to their subscription. The module is identified by the provided price ID
-func (sc *StripeClient) CreateBillingPortalAddModuleSession(ctx context.Context, subsID, custID, priceID string) (*BillingPortalSession, error) {
-	params := &stripe.BillingPortalSessionCreateParams{
-		Customer:  &custID,
-		ReturnURL: &sc.Config.StripeBillingPortalSuccessURL,
-		FlowData: &stripe.BillingPortalSessionCreateFlowDataParams{
-			Type: stripe.String(string(stripe.BillingPortalSessionFlowTypeSubscriptionUpdateConfirm)),
-			SubscriptionUpdateConfirm: &stripe.BillingPortalSessionCreateFlowDataSubscriptionUpdateConfirmParams{
-				Subscription: &subsID,
-				Items: []*stripe.BillingPortalSessionCreateFlowDataSubscriptionUpdateConfirmItemParams{
-					{
-						Price: &priceID,
-					},
-				},
-			},
-		},
-	}
-
-	sess, err := sc.Client.V1BillingPortalSessions.Create(ctx, params)
-	if err != nil {
-		return nil, err
-	}
-
-	return &BillingPortalSession{ManageSubscription: sess.URL}, nil
 }
