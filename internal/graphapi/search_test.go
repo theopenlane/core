03e58b45583752a2bab5b48659e5664a6ce13cfc
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-10-20 15:47:17 -0600

    simplify
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- internal/graphapi/search_test.go
+++ internal/graphapi/search_test.go
@@ -43,7 +43,7 @@ func TestGlobalSearch(t *testing.T) {
 		client           *testclient.TestClient
 		ctx              context.Context
 		query            string
-		expectedResults  int
+		expectResults    bool
 		expectedContacts int
 		expectedPrograms int
 		errExpected      string
@@ -53,7 +53,7 @@ func TestGlobalSearch(t *testing.T) {
 			client:           suite.client.api,
 			ctx:              testSearchUser.UserCtx,
 			query:            "A1CD2D",
-			expectedResults:  13, // this is total count of all objects searched
+			expectResults:    true,
 			expectedContacts: 10,
 			expectedPrograms: 3,
 		},
@@ -62,7 +62,7 @@ func TestGlobalSearch(t *testing.T) {
 			client:           suite.client.api,
 			ctx:              testSearchUser.UserCtx,
 			query:            "a1cd2d",
-			expectedResults:  13, // this is total count of all objects searched
+			expectResults:    true,
 			expectedContacts: 10,
 			expectedPrograms: 3,
 		},
@@ -71,7 +71,7 @@ func TestGlobalSearch(t *testing.T) {
 			client:           suite.client.api,
 			ctx:              testSearchUser.UserCtx,
 			query:            "a1cd2d con",
-			expectedResults:  10, // this is total count of all objects searched
+			expectResults:    true,
 			expectedContacts: 10,
 			expectedPrograms: 0,
 		},
@@ -80,23 +80,23 @@ func TestGlobalSearch(t *testing.T) {
 			client:           suite.client.api,
 			ctx:              testViewOnlyUser.UserCtx,
 			query:            "A1CD2D",
-			expectedResults:  10, // this is total count of all objects searched
+			expectResults:    true,
 			expectedContacts: 10,
 			expectedPrograms: 0, // no access to the programs by the view only user
 		},
 		{
-			name:            "no results",
-			client:          suite.client.api,
-			ctx:             testSearchUser.UserCtx,
-			query:           "NonExistent RAnD0M Str!ng F0r Sanity",
-			expectedResults: 0,
+			name:          "no results",
+			client:        suite.client.api,
+			ctx:           testSearchUser.UserCtx,
+			query:         "NonExistent RAnD0M Str!ng F0r Sanity",
+			expectResults: false,
 		},
 		{
-			name:            "no results, another user",
-			client:          suite.client.api,
-			ctx:             testAnotherUser.UserCtx,
-			query:           "A1CD2D",
-			expectedResults: 0,
+			name:          "no results, another user",
+			client:        suite.client.api,
+			ctx:           testAnotherUser.UserCtx,
+			query:         "A1CD2D",
+			expectResults: false,
 		},
 		{
 			name:        "empty query",
@@ -118,7 +118,7 @@ func TestGlobalSearch(t *testing.T) {
 			assert.NilError(t, err)
 			assert.Assert(t, resp != nil)
 
-			if tc.expectedResults > 0 {
+			if tc.expectResults {
 				if tc.expectedContacts > 0 {
 					assert.Assert(t, resp.Search.Contacts != nil)
 					assert.Check(t, is.Len(resp.Search.Contacts.Edges, tc.expectedContacts))
