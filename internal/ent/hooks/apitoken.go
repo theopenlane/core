89be43bc86a042495b9877e0d764fd7854fe7def
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-10-29 22:15:09 -0600

    uncomment
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- internal/ent/hooks/apitoken.go
+++ internal/ent/hooks/apitoken.go
@@ -2,12 +2,16 @@ package hooks
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"entgo.io/ent"
 	"github.com/rs/zerolog"
 	"github.com/samber/lo"
+	"github.com/theopenlane/iam/auth"
 	"github.com/theopenlane/iam/fgax"
+	"github.com/theopenlane/utils/keygen"
+	"github.com/theopenlane/utils/passwd"
 
 	"github.com/theopenlane/core/internal/ent/generated"
 	"github.com/theopenlane/core/internal/ent/generated/hook"
@@ -34,78 +38,78 @@ type mutationWithExpirationTime interface {
 func HookCreateAPIToken() ent.Hook {
 	return hook.On(func(next ent.Mutator) ent.Mutator {
 		return hook.APITokenFunc(func(ctx context.Context, m *generated.APITokenMutation) (generated.Value, error) {
-			// orgID, err := auth.GetOrganizationIDFromContext(ctx)
-			// if err != nil {
-			// 	return nil, err
-			// }
-			// // generate raw token
-			// secret := keygen.Secret()
-			// publicID := keygen.AlphaNumeric(16)
-			// prefix := "tola"
+			orgID, err := auth.GetOrganizationIDFromContext(ctx)
+			if err != nil {
+				return nil, err
+			}
+			// generate raw token
+			secret := keygen.Secret()
+			publicID := keygen.AlphaNumeric(16)
+			prefix := "tola"
 
-			// rawToken := fmt.Sprintf("%s_%s_%s", prefix, publicID, secret)
+			rawToken := fmt.Sprintf("%s_%s_%s", prefix, publicID, secret)
 
-			// // hash the secret for storage
-			// hash, err := passwd.CreateDerivedKey(secret)
-			// if err != nil {
-			// 	return nil, err
-			// }
+			// hash the secret for storage
+			hash, err := passwd.CreateDerivedKey(secret)
+			if err != nil {
+				return nil, err
+			}
 
-			// // set the hashed token for storage (backwards compatibility)
-			// m.SetToken(hash)
+			// set the hashed token for storage (backwards compatibility)
+			m.SetToken(hash)
 
-			// // set the new token_hash (argon2 of secret)
-			// m.SetTokenHash(hash)
+			// set the new token_hash (argon2 of secret)
+			m.SetTokenHash(hash)
 
-			// // set the token fingerprint for lookup
-			// tokenFP := keygen.GenerateSHA256Hmac("", []byte(publicID))
-			// m.SetTokenFingerprint(tokenFP)
+			// set the token fingerprint for lookup
+			tokenFP := keygen.GenerateSHA256Hmac("", []byte(publicID))
+			m.SetTokenFingerprint(tokenFP)
 
-			// // set organization on the token
-			// m.SetOwnerID(orgID)
+			// set organization on the token
+			m.SetOwnerID(orgID)
 
-			// if err := validateExpirationTime(m); err != nil {
-			// 	return nil, err
-			// }
+			if err := validateExpirationTime(m); err != nil {
+				return nil, err
+			}
 
 			retVal, err := next.Mutate(ctx, m)
 			if err != nil {
 				return nil, err
 			}
 
-			// token, ok := retVal.(*generated.APIToken)
-			// if !ok {
-			// 	return retVal, err
-			// }
+			token, ok := retVal.(*generated.APIToken)
+			if !ok {
+				return retVal, err
+			}
 
-			// // set the token field on the returned object to the raw token so the caller can see it
-			// token.Token = rawToken
+			// set the token field on the returned object to the raw token so the caller can see it
+			token.Token = rawToken
 
-			// // create the relationship tuples in fga for the token
-			// tuples, err := createScopeTuples(token.Scopes, orgID, token.ID)
-			// if err != nil {
-			// 	return retVal, err
-			// }
+			// create the relationship tuples in fga for the token
+			tuples, err := createScopeTuples(token.Scopes, orgID, token.ID)
+			if err != nil {
+				return retVal, err
+			}
 
-			// // add self relation tuple for the token
-			// req := fgax.TupleRequest{
-			// 	SubjectID:   token.ID,
-			// 	SubjectType: auth.ServiceSubjectType,
-			// 	ObjectID:    token.ID,
-			// 	ObjectType:  auth.ServiceSubjectType,
-			// 	Relation:    fgax.SelfRelation,
-			// }
+			// add self relation tuple for the token
+			req := fgax.TupleRequest{
+				SubjectID:   token.ID,
+				SubjectType: auth.ServiceSubjectType,
+				ObjectID:    token.ID,
+				ObjectType:  auth.ServiceSubjectType,
+				Relation:    fgax.SelfRelation,
+			}
 
-			// tuples = append(tuples, fgax.GetTupleKey(req))
+			tuples = append(tuples, fgax.GetTupleKey(req))
 
-			// // create the relationship tuples if we have any
-			// if len(tuples) > 0 {
-			// 	if _, err := m.Authz.WriteTupleKeys(ctx, tuples, nil); err != nil {
-			// 		zerolog.Ctx(ctx).Error().Err(err).Msg("failed to create relationship tuple")
+			// create the relationship tuples if we have any
+			if len(tuples) > 0 {
+				if _, err := m.Authz.WriteTupleKeys(ctx, tuples, nil); err != nil {
+					zerolog.Ctx(ctx).Error().Err(err).Msg("failed to create relationship tuple")
 
-			// 		return nil, err
-			// 	}
-			// }
+					return nil, err
+				}
+			}
 
 			return retVal, err
 		})
