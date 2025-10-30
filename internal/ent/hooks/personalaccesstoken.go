87c6af445a68f32195f17470d97ed60846a761d4
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-10-29 22:15:09 -0600

    uncomment
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- internal/ent/hooks/personalaccesstoken.go
+++ internal/ent/hooks/personalaccesstoken.go
@@ -2,11 +2,15 @@ package hooks
 
 import (
 	"context"
+	"fmt"
 
 	"entgo.io/ent"
 
 	"github.com/theopenlane/core/internal/ent/generated"
 	"github.com/theopenlane/core/internal/ent/generated/hook"
+	"github.com/theopenlane/iam/auth"
+	"github.com/theopenlane/utils/keygen"
+	"github.com/theopenlane/utils/passwd"
 )
 
 const (
@@ -17,56 +21,55 @@ const (
 func HookCreatePersonalAccessToken() ent.Hook {
 	return hook.On(func(next ent.Mutator) ent.Mutator {
 		return hook.PersonalAccessTokenFunc(func(ctx context.Context, m *generated.PersonalAccessTokenMutation) (generated.Value, error) {
-			// userID, err := auth.GetSubjectIDFromContext(ctx)
-			// if err != nil {
-			// 	return nil, err
-			// }
+			userID, err := auth.GetSubjectIDFromContext(ctx)
+			if err != nil {
+				return nil, err
+			}
 
-			// if err := validateExpirationTime(m); err != nil {
-			// 	return nil, err
-			// }
+			if err := validateExpirationTime(m); err != nil {
+				return nil, err
+			}
 
-			// // generate raw token
-			// secret := keygen.Secret()
-			// publicID := keygen.AlphaNumeric(16)
-			// prefix := "tolp"
+			// generate raw token
+			secret := keygen.Secret()
+			publicID := keygen.AlphaNumeric(16)
+			prefix := "tolp"
 
-			// rawToken := fmt.Sprintf("%s_%s_%s", prefix, publicID, secret)
+			rawToken := fmt.Sprintf("%s_%s_%s", prefix, publicID, secret)
 
-			// // hash the secret for storage
-			// hash, err := passwd.CreateDerivedKey(secret)
-			// if err != nil {
-			// 	return nil, err
-			// }
+			// hash the secret for storage
+			hash, err := passwd.CreateDerivedKey(secret)
+			if err != nil {
+				return nil, err
+			}
 
-			// // set the hashed token for storage (backwards compatibility)
-			// m.SetToken(hash)
+			// set the hashed token for storage (backwards compatibility)
+			m.SetToken(hash)
 
-			// // set the new token_hash (argon2 of secret)
-			// m.SetTokenHash(hash)
+			// set the new token_hash (argon2 of secret)
+			m.SetTokenHash(hash)
 
-			// // set the token fingerprint for lookup
-			// tokenFP := keygen.GenerateSHA256Hmac("", []byte(publicID))
-			// m.SetTokenFingerprint(tokenFP)
+			// set the token fingerprint for lookup
+			tokenFP := keygen.GenerateSHA256Hmac("", []byte(publicID))
+			m.SetTokenFingerprint(tokenFP)
 
-			// // set user on the token
-			// m.SetOwnerID(userID)
+			// set user on the token
+			m.SetOwnerID(userID)
 
 			retVal, err := next.Mutate(ctx, m)
 			if err != nil {
 				return nil, err
 			}
 
-			// // set the token field on the returned object to the raw token so the caller can see it
-			// pat, ok := retVal.(*generated.PersonalAccessToken)
-			// if !ok {
-			// 	return retVal, nil
-			// }
+			// set the token field on the returned object to the raw token so the caller can see it
+			pat, ok := retVal.(*generated.PersonalAccessToken)
+			if !ok {
+				return retVal, nil
+			}
 
-			// pat.Token = rawToken
+			pat.Token = rawToken
 
-			// return pat, nil
-			return retVal, nil
+			return pat, nil
 		})
 	}, ent.OpCreate)
 }
