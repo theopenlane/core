66acf71dda8a4f0f416adbf80e6c975bd830ad7e
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-11-07 08:46:22 -0700

    fix logger being updated with other context values
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- pkg/events/soiree/options.go
+++ pkg/events/soiree/options.go
@@ -21,7 +21,7 @@ var DefaultIDGenerator = func() string {
 
 // DefaultPanicHandler handles panics by printing the panic value
 var DefaultPanicHandler = func(p any) {
-	log.Error().Msgf("Panic occurred processing event: %v", p)
+	log.Error().Msgf("panic occurred processing event: %v", p)
 }
 
 // WithErrorHandler sets a custom error handler for an Soiree
