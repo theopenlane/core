0946e3f221ac9fed7a8c1bfa2abf048d53f22830
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-11-09 00:13:19 -0700

    remove conditional
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- internal/httpserve/handlers/webhook.go
+++ internal/httpserve/handlers/webhook.go
@@ -234,7 +234,7 @@ func (h *Handler) HandleEvent(c context.Context, e *stripe.Event) error {
 		}
 
 		err = h.handleSubscriptionPaused(c, subscription)
-		if e.Type == stripe.EventTypeCustomerSubscriptionDeleted && ent.IsNotFound(err) {
+		if ent.IsNotFound(err) {
 			// if org subscription not found, just log and move on
 			log.Info().Str("subscription_id", subscription.ID).Msg("org subscription not found for paused/deleted subscription, skipping further processing")
 
