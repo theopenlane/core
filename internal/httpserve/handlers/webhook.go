036d1c30957d0fe6a78a85e5cec8244139c3ba12
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-11-08 23:40:41 -0700

    ignore delete on deleted events
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- internal/httpserve/handlers/webhook.go
+++ internal/httpserve/handlers/webhook.go
@@ -232,7 +232,15 @@ func (h *Handler) HandleEvent(c context.Context, e *stripe.Event) error {
 			return err
 		}
 
-		return h.handleSubscriptionPaused(c, subscription)
+		err = h.handleSubscriptionPaused(c, subscription)
+		if ent.IsNotFound(err) {
+			// if org subscription not found, just log and move on
+			log.Info().Str("subscription_id", subscription.ID).Msg("org subscription not found for paused/deleted subscription, skipping further processing")
+
+			return nil
+		}
+
+		return err
 	default:
 		log.Warn().Str("event_type", string(e.Type)).Msg("unsupported event type")
 
