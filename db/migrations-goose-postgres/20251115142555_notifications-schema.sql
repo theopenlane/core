ac28de7a273690a0a9f3c0e906a3b277c83327a7
manab-pr <manab2001maity@gmail.com>
2025-11-17 21:30:43 +0530

    refactor: combine notification schema migrations into single file

--- db/migrations-goose-postgres/20251115142555_notifications-schema.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
--- modify "notifications" table
-ALTER TABLE "notifications" DROP CONSTRAINT "notifications_users_notifications", ALTER COLUMN "user_id" DROP NOT NULL, ADD CONSTRAINT "notifications_users_notifications" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
-
--- +goose Down
--- reverse: modify "notifications" table
-ALTER TABLE "notifications" DROP CONSTRAINT "notifications_users_notifications", ALTER COLUMN "user_id" SET NOT NULL, ADD CONSTRAINT "notifications_users_notifications" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
--- db/migrations/20251115142553_notifications-schema.sql
+++
@@ -1,2 +0,0 @@
--- Modify "notifications" table
-ALTER TABLE "notifications" DROP CONSTRAINT "notifications_users_notifications", ALTER COLUMN "user_id" DROP NOT NULL, ADD CONSTRAINT "notifications_users_notifications" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
