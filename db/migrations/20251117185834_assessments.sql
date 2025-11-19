58fa4667fd7178c49a83b07696131c55776b7912
Lanre Adelowo <yo@lanre.wtf>
2025-11-19 17:51:33 +0100

    squash migrations

--- db/migrations/20251117185834_assessments.sql
+++
@@ -1,8 +0,0 @@
--- Modify "assessment_history" table
-ALTER TABLE "assessment_history" DROP COLUMN "assessment_owner_id", ADD COLUMN "response_due_duration" bigint NOT NULL DEFAULT 604800;
--- Modify "assessment_response_history" table
-ALTER TABLE "assessment_response_history" ALTER COLUMN "status" SET DEFAULT 'SENT';
--- Modify "assessment_responses" table
-ALTER TABLE "assessment_responses" ALTER COLUMN "status" SET DEFAULT 'SENT';
--- Modify "assessments" table
-ALTER TABLE "assessments" DROP CONSTRAINT "assessments_templates_template", DROP COLUMN "assessment_owner_id", ADD COLUMN "response_due_duration" bigint NOT NULL DEFAULT 604800, ADD CONSTRAINT "assessments_templates_assessments" FOREIGN KEY ("template_id") REFERENCES "templates" ("id") ON UPDATE NO ACTION ON DELETE NO ACTION;
--- db/migrations/20251119161704_assessments.sql
+++
@@ -1,8 +0,0 @@
--- Modify "assessment_history" table
-ALTER TABLE "assessment_history" ALTER COLUMN "template_id" DROP NOT NULL, ALTER COLUMN "response_due_duration" DROP NOT NULL, ALTER COLUMN "response_due_duration" DROP DEFAULT, ADD COLUMN "jsonconfig" jsonb NULL, ADD COLUMN "uischema" jsonb NULL;
--- Modify "document_data_history" table
-ALTER TABLE "document_data_history" ALTER COLUMN "template_id" DROP NOT NULL;
--- Modify "assessments" table
-ALTER TABLE "assessments" DROP CONSTRAINT "assessments_templates_assessments", ALTER COLUMN "template_id" DROP NOT NULL, ALTER COLUMN "response_due_duration" DROP NOT NULL, ALTER COLUMN "response_due_duration" DROP DEFAULT, ADD COLUMN "jsonconfig" jsonb NULL, ADD COLUMN "uischema" jsonb NULL, ADD CONSTRAINT "assessments_templates_assessments" FOREIGN KEY ("template_id") REFERENCES "templates" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
--- Modify "document_data" table
-ALTER TABLE "document_data" DROP CONSTRAINT "document_data_templates_documents", ALTER COLUMN "template_id" DROP NOT NULL, ADD CONSTRAINT "document_data_templates_documents" FOREIGN KEY ("template_id") REFERENCES "templates" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
