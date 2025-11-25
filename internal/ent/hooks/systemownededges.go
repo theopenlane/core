69ca41e29cc47131d8a44bdd849f1415df1997b5
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2025-11-25 15:57:55 -0700

    cleanup
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- internal/ent/hooks/systemownededges.go
+++ internal/ent/hooks/systemownededges.go
@@ -19,7 +19,7 @@ type controlEdgeMutation interface {
 	SubcontrolsIDs() []string
 }
 
-// HookSystemOwnedControls runs on evidence mutations to check for system owned controls
+// HookSystemOwnedControls runs on mutations to check for system owned controls
 // since only view access to a control is required for edges on tasks, evidence, this
 // ensures that system owned controls are not linked to org owned objects
 func HookSystemOwnedControls() ent.Hook {
@@ -31,7 +31,6 @@ func HookSystemOwnedControls() ent.Hook {
 
 			mut, ok := m.(controlEdgeMutation)
 			if !ok {
-
 				// nothing to do if its not the right mutation type
 				return next.Mutate(ctx, m)
 			}
@@ -51,18 +50,18 @@ func checkControlEdges(ctx context.Context, m controlEdgeMutation) error {
 	controlIDs := m.ControlsIDs()
 
 	if len(controlIDs) > 0 {
-		controlEdges, err := m.Client().Control.Query().Where(
+		systemOwnedControlCount, err := m.Client().Control.Query().Where(
 			control.IDIn(controlIDs...),
 			control.SystemOwnedEQ(true),
-		).All(ctx)
+		).Count(ctx)
 		if err != nil {
-			logx.FromContext(ctx).Error().Err(err).Msg("failed to query controls for evidence mutation")
+			logx.FromContext(ctx).Error().Err(err).Msgf("failed to query controls for %s mutation", m.Type())
 
 			return err
 		}
 
-		if len(controlEdges) > 0 {
-			logx.FromContext(ctx).Warn().Msg("permission denied: attempt to link system owned control to evidence")
+		if systemOwnedControlCount > 0 {
+			logx.FromContext(ctx).Warn().Msgf("permission denied: attempt to link system owned control to %s", m.Type())
 
 			return generated.ErrPermissionDenied
 		}
@@ -72,17 +71,18 @@ func checkControlEdges(ctx context.Context, m controlEdgeMutation) error {
 	subcontrolIDs := m.SubcontrolsIDs()
 
 	if len(subcontrolIDs) > 0 {
-		subcontrolEdges, err := m.Client().Subcontrol.Query().Where(
+		systemOwnedControlCount, err := m.Client().Subcontrol.Query().Where(
 			subcontrol.IDIn(subcontrolIDs...),
 			subcontrol.SystemOwnedEQ(true),
-		).All(ctx)
+		).Count(ctx)
 		if err != nil {
-			logx.FromContext(ctx).Error().Err(err).Msg("failed to query subcontrols for evidence mutation")
+			logx.FromContext(ctx).Error().Err(err).Msgf("failed to query subcontrols for %s mutation", m.Type())
+
 			return err
 		}
 
-		if len(subcontrolEdges) > 0 {
-			logx.FromContext(ctx).Warn().Msg("permission denied: attempt to link system owned subcontrol to evidence")
+		if systemOwnedControlCount > 0 {
+			logx.FromContext(ctx).Warn().Msgf("permission denied: attempt to link system owned subcontrol to %s", m.Type())
 
 			return generated.ErrPermissionDenied
 		}
