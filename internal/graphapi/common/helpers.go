c2729ac31eea2ccb1d956cbd11f2c23995e7c0a9
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2026-01-06 08:53:10 -0600

    fix linter

--- internal/graphapi/common/helpers.go
+++ internal/graphapi/common/helpers.go
@@ -444,7 +444,7 @@ func enumInfoForType(t reflect.Type, cache map[reflect.Type]enumInfo) enumInfo {
 	}
 
 	method, ok := t.MethodByName("Values")
-	receiver := reflect.Value{}
+	receiver := reflect.Value{} //nolint:ineffassign
 	if ok {
 		// Value receiver provides Values()
 		receiver = reflect.Zero(t)
