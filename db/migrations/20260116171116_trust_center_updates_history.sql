6289bf3a20874914c6bd41e655b99024dbc031a7
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2026-01-16 10:45:37 -0700

    correct migration files
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- db/migrations/20260116171116_trust_center_updates_history.sql
+++ db/migrations/20260116171116_trust_center_updates_history.sql
@@ -1,4 +1,2 @@
 -- Create "trust_center_entity_history" table
 CREATE TABLE "trust_center_entity_history" ("id" character varying NOT NULL, "history_time" timestamptz NOT NULL, "ref" character varying NULL, "operation" character varying NOT NULL, "created_at" timestamptz NULL, "updated_at" timestamptz NULL, "created_by" character varying NULL, "updated_by" character varying NULL, "deleted_at" timestamptz NULL, "deleted_by" character varying NULL, "logo_file_id" character varying NULL, "url" character varying NULL, "trust_center_id" character varying NULL, "name" character varying NOT NULL, "entity_type_id" character varying NULL, PRIMARY KEY ("id"));
--- Create index "trustcenterentityhistory_history_time" to table: "trust_center_entity_history"
-CREATE INDEX "trustcenterentityhistory_history_time" ON "trust_center_entity_history" ("history_time");
