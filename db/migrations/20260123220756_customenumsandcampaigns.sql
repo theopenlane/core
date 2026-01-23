af1fec5c1a58140ac6cc4938a988d04c17eafefa
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2026-01-23 16:35:51 -0600

    migration fix

--- db/migrations/20260123220756_customenumsandcampaigns.sql
+++
@@ -1,2 +0,0 @@
--- Modify "entities" table
-ALTER TABLE "entities" ALTER COLUMN "review_frequency" SET DEFAULT 'YEARLY';
--- db/migrations/20260123220759_customenumsandcampaigns_history.sql
+++
@@ -1,2 +0,0 @@
--- Modify "entity_history" table
-ALTER TABLE "entity_history" ALTER COLUMN "review_frequency" SET DEFAULT 'YEARLY';
