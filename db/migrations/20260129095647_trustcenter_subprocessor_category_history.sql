026f0f9f5aa5c4cc9aaa72a3dc9fe28ed780ebf5
Lanre Adelowo <yo@lanre.wtf>
2026-02-01 02:26:30 +0100

    task regenerate

--- db/migrations/20260129095647_trustcenter_subprocessor_category_history.sql
+++
@@ -1,2 +0,0 @@
--- Modify "trust_center_subprocessor_history" table
-ALTER TABLE "trust_center_subprocessor_history" DROP COLUMN "category", ADD COLUMN "trust_center_subprocessor_kind_name" character varying NULL, ADD COLUMN "trust_center_subprocessor_kind_id" character varying NULL;
