0eec42313d3d71fbd0c0e4fb58e894a3fc814e1f
Lanre Adelowo <yo@lanre.wtf>
2026-02-16 17:41:46 +0100

    fix trustcenter scoping

---
+++ internal/ent/schemautil/trustcenter_scope.go
@@ -0,0 +1,51 @@
+package schemautil
+
+import (
+	"entgo.io/ent/dialect/sql"
+
+	"github.com/theopenlane/iam/auth"
+
+	"github.com/theopenlane/core/internal/ent/generated/trustcenter"
+)
+
+// TrustCenterScopePredicate returns a predicate that scopes trust center joins
+// and filters out correctly using the trustcenter_id
+func TrustCenterScopePredicate() func(*sql.Selector) {
+	return func(s *sql.Selector) {
+		ctx := s.Context()
+
+		if auth.IsSystemAdminFromContext(ctx) {
+			return
+		}
+
+		if anon, ok := auth.AnonymousTrustCenterUserFromContext(ctx); ok {
+			if anon.TrustCenterID != "" {
+				s.Where(sql.EQ(s.C("trust_center_id"), anon.TrustCenterID))
+				return
+			}
+		}
+
+		orgIDs, err := auth.GetOrganizationIDsFromContext(ctx)
+		if err != nil || len(orgIDs) == 0 {
+			return
+		}
+
+		t := sql.Table(trustcenter.Table)
+
+		anys := make([]any, len(orgIDs))
+		for i, id := range orgIDs {
+			anys[i] = id
+		}
+
+		s.Where(
+			sql.In(
+				s.C("trust_center_id"),
+				sql.Select(t.C(trustcenter.FieldID)).From(t).Where(
+					sql.In(
+						t.C(trustcenter.FieldOwnerID), anys...,
+					),
+				),
+			),
+		)
+	}
+}
