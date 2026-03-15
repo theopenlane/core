93b7cbda0381702e4d3fe37e61066587c5388f37
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2026-03-15 08:55:47 -0500

    updated generate output

--- internal/ent/generated/client.go
+++ internal/ent/generated/client.go
@@ -138,6 +138,7 @@ import (
 	"github.com/theopenlane/iam/sessions"
 	"github.com/theopenlane/iam/tokens"
 	"github.com/theopenlane/iam/totp"
+	"github.com/theopenlane/newman/compose"
 	"gocloud.dev/secrets"
 
 	"github.com/theopenlane/iam/entfga"
