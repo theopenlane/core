d379ba7bc52e864228621f02c941330ea42132e9
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2026-03-16 14:58:57 -0500

    collapse migrations

--- db/migrations/20260311182352_emailtemplates.sql
+++
@@ -1,6 +0,0 @@
--- Modify "email_templates" table
-ALTER TABLE "email_templates" ADD COLUMN "revision" character varying NULL DEFAULT 'v0.0.1', ADD COLUMN "template_context" character varying NULL;
--- Modify "notification_templates" table
-ALTER TABLE "notification_templates" ADD COLUMN "revision" character varying NULL DEFAULT 'v0.0.1', ADD COLUMN "template_context" character varying NULL;
--- Modify "files" table
-ALTER TABLE "files" ADD COLUMN "email_template_files" character varying NULL, ADD CONSTRAINT "files_email_templates_files" FOREIGN KEY ("email_template_files") REFERENCES "email_templates" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
