efc8efd91584022d527c6648089973fb3e94a114
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2026-03-16 14:58:57 -0500

    collapse migrations

--- db/migrations/20260311182356_emailtemplates_history.sql
+++
@@ -1,4 +0,0 @@
--- Modify "email_template_history" table
-ALTER TABLE "email_template_history" ADD COLUMN "revision" character varying NULL DEFAULT 'v0.0.1', ADD COLUMN "template_context" character varying NULL;
--- Modify "notification_template_history" table
-ALTER TABLE "notification_template_history" ADD COLUMN "revision" character varying NULL DEFAULT 'v0.0.1', ADD COLUMN "template_context" character varying NULL;
