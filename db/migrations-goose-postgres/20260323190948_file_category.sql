38cf2924c2e6d37fc8a90215fedae7e48b5b1030
Lanre Adelowo <yo@lanre.wtf>
2026-03-26 00:15:22 +0100

    add extended file

--- db/migrations-goose-postgres/20260323190948_file_category.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
--- modify "files" table
-ALTER TABLE "files" ADD COLUMN "file_category_name" character varying NULL, ADD COLUMN "file_category_id" character varying NULL, ADD CONSTRAINT "files_custom_type_enums_file_category" FOREIGN KEY ("file_category_id") REFERENCES "custom_type_enums" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
-
--- +goose Down
--- reverse: modify "files" table
-ALTER TABLE "files" DROP CONSTRAINT "files_custom_type_enums_file_category", DROP COLUMN "file_category_id", DROP COLUMN "file_category_name";
--- db/migrations-goose-postgres/20260323190957_file_category_history.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
--- modify "file_history" table
-ALTER TABLE "file_history" ADD COLUMN "file_category_name" character varying NULL, ADD COLUMN "file_category_id" character varying NULL;
-
--- +goose Down
--- reverse: modify "file_history" table
-ALTER TABLE "file_history" DROP COLUMN "file_category_id", DROP COLUMN "file_category_name";
