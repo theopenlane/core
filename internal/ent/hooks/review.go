ca5d53bd38e6497267f8b7376330ca009e97c629
Lanre Adelowo <yo@lanre.wtf>
2026-03-27 21:42:56 +0100

    update entity based off review

--- internal/ent/hooks/review.go
+++ internal/ent/hooks/review.go
@@ -5,8 +5,10 @@ import (
 
 	"entgo.io/ent"
 
+	"github.com/theopenlane/core/common/models"
 	"github.com/theopenlane/core/internal/ent/generated"
 	"github.com/theopenlane/core/internal/ent/generated/hook"
+	"github.com/theopenlane/core/pkg/logx"
 	pkgobjects "github.com/theopenlane/core/pkg/objects"
 )
 
@@ -26,7 +28,41 @@ func HookReviewFiles() ent.Hook {
 				m.AddFileIDs(fileIDs...)
 			}
 
-			return next.Mutate(ctx, m)
+			v, err := next.Mutate(ctx, m)
+			if err != nil {
+				return nil, err
+			}
+
+			entityIDs := m.EntitiesIDs()
+			if len(entityIDs) == 0 {
+				return v, nil
+			}
+
+			userID, exists := m.CreatedBy()
+			createdAt, createdAtExists := m.CreatedAt()
+
+			for _, id := range entityIDs {
+				q := m.Client().Entity.UpdateOneID(id)
+
+				if exists {
+					q = q.SetReviewedBy(userID)
+				}
+
+				if createdAtExists {
+					q = q.SetLastReviewedAt(models.DateTime(createdAt))
+				}
+
+				err := q.Exec(ctx)
+				if err != nil {
+					logx.FromContext(ctx).Err(err).
+						Str("entity_id", id).
+						Msg("could not update entity reviewer")
+					return nil, err
+				}
+
+			}
+
+			return v, nil
 		})
 	}, ent.OpCreate|ent.OpUpdateOne|ent.OpUpdate)
 }
