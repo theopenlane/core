750cb484f75f1fcf263930e573b2cacf08cbc988
Lanre Adelowo <yo@lanre.wtf>
2026-04-07 23:58:24 +0100

    task regenerate and fix build issues

--- db/migrations/20260407204420_file_category_history.sql
+++
@@ -1,2 +0,0 @@
--- Modify "file_history" table
-ALTER TABLE "file_history" ADD COLUMN "file_category_name" character varying NULL, ADD COLUMN "file_category_id" character varying NULL;
--- db/migrations/20260407223333_file_category_history.sql
+++
@@ -1,2 +0,0 @@
--- Modify "file_history" table
-ALTER TABLE "file_history" DROP COLUMN "file_category_name", DROP COLUMN "file_category_id", ADD COLUMN "category_name" character varying NULL, ADD COLUMN "category_id" character varying NULL;
