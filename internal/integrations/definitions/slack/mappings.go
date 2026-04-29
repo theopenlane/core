e9c5e7178e82696176e4e59a0f1fcde561b85a07
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2026-04-29 12:37:05 -0600

    fix bad update
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- internal/integrations/definitions/slack/mappings.go
+++ internal/integrations/definitions/slack/mappings.go
@@ -8,7 +8,7 @@ import (
 
 // mapExprDirectoryAccount is the CEL mapping expression for Slack workspace user payloads mapped to DirectoryAccount
 var mapExprDirectoryAccount = providerkit.CelMapExpr([]providerkit.CelMapEntry{
-	{Key: integrationgenerated.IntegrationMappingDirectoryAccountExternalID, Expr: `payload.id : ""`},
+	{Key: integrationgenerated.IntegrationMappingDirectoryAccountExternalID, Expr: `payload.id`},
 	{Key: integrationgenerated.IntegrationMappingDirectoryAccountCanonicalEmail, Expr: `'email' in payload ? payload.email : ""`},
 	{Key: integrationgenerated.IntegrationMappingDirectoryAccountDisplayName, Expr: `'display_name' in payload && payload.display_name != "" ? payload.display_name : ('real_name' in payload && payload.real_name != "" ? payload.real_name : ('name' in payload ? payload.name : ""))`},
 	{Key: integrationgenerated.IntegrationMappingDirectoryAccountGivenName, Expr: `'first_name' in payload ? payload.first_name : ""`},
@@ -17,7 +17,7 @@ var mapExprDirectoryAccount = providerkit.CelMapExpr([]providerkit.CelMapEntry{
 	{Key: integrationgenerated.IntegrationMappingDirectoryAccountAvatarRemoteURL, Expr: `'avatar_url' in payload ? payload.avatar_url : ""`},
 	{Key: integrationgenerated.IntegrationMappingDirectoryAccountMfaState, Expr: `dyn(payload.has_2fa ? "ENABLED" : "DISABLED")`},
 	{Key: integrationgenerated.IntegrationMappingDirectoryAccountStatus, Expr: `dyn(payload.deleted ? "INACTIVE" : "ACTIVE")`},
-	{Key: integrationgenerated.IntegrationMappingDirectoryAccountDirectoryInstanceID, Expr: `payload.team_id : ""`},
+	{Key: integrationgenerated.IntegrationMappingDirectoryAccountDirectoryInstanceID, Expr: `payload.team_id`},
 	{Key: integrationgenerated.IntegrationMappingDirectoryAccountProfile, Expr: "payload"},
 	{Key: integrationgenerated.IntegrationMappingDirectoryAccountAccountType, Expr: `payload.is_bot ? "SERVICE" : payload.is_external ? "GUEST" : "USER"`},
 })
