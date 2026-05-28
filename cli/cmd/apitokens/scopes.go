3bd5301acc8b3645edfb88c0f3ba5018a9d71b8e
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2026-05-28 15:37:39 -0600

    formatting
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- cli/cmd/apitokens/scopes.go
+++ cli/cmd/apitokens/scopes.go
@@ -23,6 +23,11 @@ func init() {
 	command.AddCommand(scopesCmd)
 }
 
+const (
+	boldStart = "\033[1m"
+	boldEnd   = "\033[0m"
+)
+
 // allScopes returns all available scopes
 func allScopes() error {
 	scopes, err := fgamodel.ScopeOptions()
@@ -36,10 +41,10 @@ func allScopes() error {
 	}
 	sort.Strings(objects)
 
-	desc := fmt.Sprintf("available scopes: \n\n")
+	desc := fmt.Sprintf(boldStart + "Available Scopes: \n\n" + boldEnd)
 	for _, obj := range objects {
 		for _, v := range scopes[obj] {
-			desc += fmt.Sprintf("%s:%s ", obj, v)
+			desc += fmt.Sprintf(boldStart+"%s"+boldEnd+":%s ", obj, v)
 		}
 		desc += fmt.Sprintf("\n")
 	}
