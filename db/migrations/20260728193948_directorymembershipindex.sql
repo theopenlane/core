63c325bed8a1b1879dd86339fcdd59198387a2f7
Matt Anderson <42154938+matoszz@users.noreply.github.com>
2026-07-28 22:40:34 -0500

    Repair DirectorySync (#2645)
    * migrations for directorysync
    
    * query interceptor

---
+++ db/migrations/20260728193948_directorymembershipindex.sql
@@ -0,0 +1,4 @@
+-- Drop index "directorymembership_directory_account_id_directory_group_id" from table: "directory_memberships"
+DROP INDEX "directorymembership_directory_account_id_directory_group_id";
+-- Create index "directorymembership_directory_account_id_directory_group_id" to table: "directory_memberships"
+CREATE UNIQUE INDEX "directorymembership_directory_account_id_directory_group_id" ON "directory_memberships" ("directory_account_id", "directory_group_id") WHERE (removed_at IS NULL);
