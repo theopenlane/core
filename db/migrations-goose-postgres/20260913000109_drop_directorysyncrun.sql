af8928fa46d2477e27830c0a6ba8d9cf840ec540
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2026-09-16 10:17:07 -0500

    regenerate migrations and reset hash

--- db/migrations-goose-postgres/20260913000109_drop_directorysyncrun.sql
+++
@@ -1,6 +0,0 @@
--- +goose Up
--- drop "directory_sync_runs" table
-DROP TABLE IF EXISTS "directory_sync_runs" CASCADE;
-
--- +goose Down
--- irreversible: the dropped table cannot be restored from this migration
--- db/migrations/20260913000101_drop_directorysyncrun.sql
+++
@@ -1,2 +0,0 @@
--- Drop "directory_sync_runs" table
-DROP TABLE IF EXISTS "directory_sync_runs" CASCADE;
